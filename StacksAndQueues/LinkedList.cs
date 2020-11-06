36f21cb37e317594221cbde86bef8540bd5f6096
Ramya.U <ramyayadav2016@gmail.com>
2020-11-06 23:25:42 +0530

    [Ramya] Add . Added enqueue method

---
+++ StacksAndQueues/LinkedList.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StacksAndQueues
+{
+    public class LinkedList
+    {
+        Node head = null;
+        /// <summary>
+        /// Enqueues the specified data.
+        /// </summary>
+        /// <param name="data">The data.</param>
+        internal void Enqueue(int data)
+        {
+            Node node = new Node(data);
+            if (head == null)
+                head = node;
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+            Console.WriteLine("{0} inserted into queue ", node.data);
+        }
+
+        /// <summary>
+        /// Displays this instance.
+        /// </summary>
+        public void Display()
+        {
+            if (head == null)
+            {
+                Console.WriteLine("Queue is Empty");
+            }
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    Console.Write(temp.data + " ");
+                    temp = temp.next;
+                }
+                Console.Write(temp.data + "\n");
+            }
+        }
+    }
+}
--- StacksAndQueues/Program.cs
+++ StacksAndQueues/Program.cs
@@ -4,9 +4,18 @@ namespace StacksAndQueues
 {
     class Program
     {
+        /// <summary>
+        /// Defines the entry point of the application.
+        /// </summary>
+        /// <param name="args">The arguments.</param>
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, Welcome to Stacks and Queues!");
+            LinkedList list = new LinkedList();
+            list.Enqueue(56);
+            list.Enqueue(30);
+            list.Enqueue(70);
+            list.Display();
         }
     }
 }
