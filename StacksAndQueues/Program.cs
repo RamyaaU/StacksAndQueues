bd9fc22f8d19fa73dfcee4c58e4f62b6ada05182
Ramya.U <ramyayadav2016@gmail.com>
2020-11-06 23:31:04 +0530

    [Ramya] Add . Added Dequeue from the beginning

--- StacksAndQueues/Program.cs
+++ StacksAndQueues/Program.cs
@@ -7,6 +7,12 @@ namespace StacksAndQueues
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, Welcome to Stacks and Queues!");
+            LinkedList list = new LinkedList();
+            list.Enqueue(56);
+            list.Enqueue(30);
+            list.Enqueue(70);
+            list.Dequeue();
+            list.Display();
         }
     }
 }
